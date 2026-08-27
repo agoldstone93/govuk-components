ab4c8622da2721b004a3ea38b7752613692caf07
Peter Yates <peter.yates@graphia.co.uk>
2026-08-27 18:59:42 +0100

    Rejig service nav component's slots, add inline
    Previously this library only implemented the start_slot and end_slot
    which were rendered on either side of the navigation.
    
    Now there are four slots:
    
    * content_before (rendered first in the outer width container)
    * navigation_start (rendered first inside the <nav> element)
    * navigation_end (rendered last inside the <nav> element)
    * content_after (rendered last in the outer width container)
    
    The original implementation of this didn't take all of the slots into
    account.
    
    They are laid out like this:
    
      ┌────────────────────────────────────────────┐
      │              CONTENT BEFORE                │
      └────────────────────────────────────────────┘
      ┌──────────┐┌────────────────────────────────┐
      │  SERVICE ││┌───┐┌────────────────────┐┌───┐│
      │   NAME   │││ S ││        NAV         ││ E ││
      │          ││└───┘└────────────────────┘└───┘│
      └──────────┘└────────────────────────────────┘
      ┌────────────────────────────────────────────┐
      │              CONTENT AFTER                 │
      └────────────────────────────────────────────┘
    
    However, the service nav can now be laid out like this, by passing
    inline: true to the component:
    
      ┌────────────────────────────────────────────┐
      │              CONTENT BEFORE                │
      └────────────────────────────────────────────┘
      ┌──────────┐┌───────────────────┐ ┌──────────┐
      │  SERVICE ││┌───┐┌───────┐┌───┐│ │ CONTENT  │
      │   NAME   │││ S ││  NAV  ││ E ││ │  AFTER   │
      │          ││└───┘└───────┘└───┘│ │          │
      └──────────┘└───────────────────┘ └──────────┘
    
    Differences from the upstream implementation:
    
    - we pass `inline: true` rather `align: "inline"` as it's more consistent with
      the rest of the library
    - `end` is a reserved keyword in Rails so the start/end in
      govuk-frontend are replaced with before/after here. The ViewComponent
      API means slots are called with a `with_` prefix, so the full name
      `with_content_before` was chosen over `with_before`

--- app/components/govuk_component/service_navigation_component.rb
+++ app/components/govuk_component/service_navigation_component.rb
@@ -1,6 +1,8 @@
 class GovukComponent::ServiceNavigationComponent < GovukComponent::Base
-  renders_one :start_slot
-  renders_one :end_slot
+  renders_one :navigation_start
+  renders_one :navigation_end
+  renders_one :content_before
+  renders_one :content_after
 
   renders_one :service_name, "GovukComponent::ServiceNavigationComponent::ServiceNameComponent"
   renders_many :navigation_items, ->(text:, href: nil, current_path: nil, active_when: nil, current: false, active: false, classes: [], html_attributes: {}) do
@@ -16,15 +18,16 @@ class GovukComponent::ServiceNavigationComponent < GovukComponent::Base
     )
   end
 
-  attr_reader :aria_label_text, :navigation_id, :inverse
+  attr_reader :aria_label_text, :navigation_id, :inverse, :inline
 
-  def initialize(service_name: nil, service_url: nil, navigation_items: [], current_path: nil, aria_label: "Service information", navigation_id: 'navigation', inverse: false, classes: [], html_attributes: {})
+  def initialize(service_name: nil, service_url: nil, navigation_items: [], current_path: nil, aria_label: "Service information", navigation_id: 'navigation', inverse: false, inline: false, classes: [], html_attributes: {})
     @service_name_text = service_name
     @service_url = service_url
     @current_path = current_path
     @aria_label_text = aria_label
     @navigation_id = navigation_id
     @inverse = inverse
+    @inline = inline
 
     if @service_name_text.present?
       with_service_name(service_name: @service_name_text, service_url:)
@@ -37,17 +40,21 @@ class GovukComponent::ServiceNavigationComponent < GovukComponent::Base
 
   def call
     outer_element do
-      tag.div(class: "#{brand}-width-container") do
-        safe_join(
-          [
-            start_slot,
-            tag.div(class: "#{brand}-service-navigation__container") do
-              safe_join([service_name, navigation].compact)
-            end,
-            end_slot
-          ]
-        )
-      end
+      safe_join(
+        [
+          tag.div(class: class_names("#{brand}-width-container", "#{brand}-service-navigation__inlining-container" => inline)) do
+            safe_join(
+              [
+                content_before,
+                tag.div(class: "#{brand}-service-navigation__container") do
+                  safe_join([service_name, navigation].compact)
+                end,
+                content_after
+              ]
+            )
+          end
+        ]
+      )
     end
   end
 
@@ -60,7 +67,7 @@ class GovukComponent::ServiceNavigationComponent < GovukComponent::Base
   end
 
   def navigation_list
-    tag.ul(safe_join(navigation_items), id: navigation_id, class: "#{brand}-service-navigation__list")
+    tag.ul(safe_join([navigation_start, *navigation_items, navigation_end]), id: navigation_id, class: "#{brand}-service-navigation__list")
   end
 
 private
